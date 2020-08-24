fe9e36e658b6538f21c7ab6d680c7ee6d2a2f947
cxq <caoxiaoqiang@bigo.sg>
2020-08-24 12:27:11 +0800

    map

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "include/Triangle.h"
+#include "map"
 
 void call_triangle() {
     Triangle triangle = Triangle(3, 4);
@@ -21,11 +22,51 @@ void aplusplus_test() {
     std::cout << c << std::endl;
 }
 
+void outputMap() {
+    std::map<std::string, std::string> map;
+    map["a"] = "100";
+    map["c"] = "200";
+    map["b"] = "300";
+
+    if (!map.empty()) {
+        for (auto iter = map.begin(); iter != map.end(); iter++) {
+            std::cout << "k=" << iter->first << ", v=" << iter->second << std::endl;
+        }
+    }
+
+    std::cout << "---------------map.size=" << map.size() << std::endl;
+
+    for (auto &item: map) {
+        std::cout << "k=" << item.first << ", v=" << item.second << std::endl;
+    }
+
+    if (map.find("a") != map.end()) {
+        std::cout << "a=" << map.at("a") << std::endl;
+    }
+
+    if (map.find("d") != map.end()) {
+        std::cout << "d=" << map.at("d") << std::endl;
+    } else {
+        std::cout << "d not in map" << std::endl;
+    }
+
+    if (map.count("d")) {
+        std::cout << "d=" << map.at("d") << std::endl;
+    } else {
+        std::cout << "d not in map" << std::endl;
+    }
+
+    std::map<std::string, std::string> map1;
+    std::cout << map1.count("a") << std::endl;
+
+
+}
 
 int main(int argc, char **argv) {
     std::cout << "[Debug]" << std::endl;
     call_triangle();
     aplusplus_test();
+    outputMap();
 
     return 0;
 }
