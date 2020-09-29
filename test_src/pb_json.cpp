8263a599a135279b8fa0c329b925593d17cf48ce
cxq <caoxiaoqiang@bigo.sg>
2020-09-29 17:55:55 +0800

    pb json switch

--- test_src/pb_json.cpp
+++ test_src/pb_json.cpp
@@ -63,8 +63,47 @@ void pb2json() {
 
 }
 
+void pb2json2() {
+    std::string json_string;
+
+    // Create a msg.
+    pkg::helloworld msg;
+    msg.set_student_name("cxq");
+    msg.add_school_id(1);
+    msg.add_school_id(2);
+    msg.set_day(pkg::helloworld_weekday_sunday);
+    auto ptr = msg.mutable_submess();
+    ptr->set_age(36);
+    ptr->set_name("james");
+
+    // Create a json_string from msg. [pb to json]
+    google::protobuf::util::JsonPrintOptions options;
+//    options.add_whitespace = true;
+    options.always_print_primitive_fields = true;
+    options.preserve_proto_field_names = true;
+    MessageToJsonString(msg, &json_string, options);
+
+    // Print json_string.
+    std::cout << json_string << std::endl;
+
+    /*
+        add_whitespace
+        (bool) 是否添加空格，换行符和缩进以使输出的JSON易于阅读。默认为false。
+
+        always_print_primitive_fields
+        (bool) 是否始终打印原始字段。默认情况下，具有默认值的原始字段将在JSON输出中被省略。例如，设置为0的int32字段将被省略。将此标志设置为true，将覆盖默认行为并打印原始字段，而不考虑其值。默认为false。
+
+        always_print_enums_as_ints
+        (bool) 是否始终打印枚举作为整数。默认情况下，它们呈现为字符串。默认为false。
+
+        preserve_proto_field_names
+        (bool) 是否保留原始字段名称。默认情况下，protobuf将使用json_name选项生成JSON字段名称，或者按照下面的顺序生成较低的骆驼风格的大小写。设置此标志将保留原始字段名称。默认为false。
+      */
+
+}
+
 
 int main(int argc, char **argv) {
-    pb2json();
+    pb2json2();
     return 0;
 }
