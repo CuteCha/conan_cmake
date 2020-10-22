e93a1124a641d26de8f22950d6071c301a7f78ac
cxq <caoxiaoqiang@bigo.sg>
2020-10-22 17:07:15 +0800

    303

--- test_src/test.cpp
+++ test_src/test.cpp
@@ -296,6 +296,158 @@ void outputMap202() {
     }
 }
 
+void outputMap303() {
+    std::map<std::string, int> map = {
+            {"exposure_time_1d", 1},
+            {"exposure_time_3d", 2},
+            {"exposure_time_7d", 3},
+            {"exposure_time_15d", 4},
+            {"exposure_time_30d", 5},
+
+            {"enter_time_1d", 6},
+            {"enter_time_3d", 7},
+            {"enter_time_7d", 8},
+            {"enter_time_15d", 9},
+            {"enter_time_30d", 10},
+
+            {"watch_time_1d", 11},
+            {"watch_time_3d", 12},
+            {"watch_time_7d", 13},
+            {"watch_time_15d", 14},
+            {"watch_time_30d", 15},
+
+            {"effective_enter_time_1d", 16},
+            {"effective_enter_time_3d", 17},
+            {"effective_enter_time_7d", 18},
+            {"effective_enter_time_15d", 19},
+            {"effective_enter_time_30d", 20},
+
+            {"effective_watch_time_1d", 21},
+            {"effective_watch_time_3d", 22},
+            {"effective_watch_time_7d", 23},
+            {"effective_watch_time_15d", 24},
+            {"effective_watch_time_30d", 25},
+
+            {"heart_cnt_1d", 26},
+            {"heart_cnt_3d", 27},
+            {"heart_cnt_7d", 28},
+            {"heart_cnt_15d", 29},
+            {"heart_cnt_30d", 30},
+
+            {"bullet_cnt_1d", 31},
+            {"bullet_cnt_3d", 32},
+            {"bullet_cnt_7d", 33},
+            {"bullet_cnt_15d", 34},
+            {"bullet_cnt_30d", 35},
+
+            {"msg_cnt_1d", 36},
+            {"msg_cnt_3d", 37},
+            {"msg_cnt_7d", 38},
+            {"msg_cnt_15d", 39},
+            {"msg_cnt_30d", 40},
+
+            {"follow_1d", 41},
+            {"follow_3d", 42},
+            {"follow_7d", 43},
+            {"follow_15d", 44},
+            {"follow_30d", 45},
+
+            {"share_cnt_1d", 46},
+            {"share_cnt_3d", 47},
+            {"share_cnt_7d", 48},
+            {"share_cnt_15d", 49},
+            {"share_cnt_30d", 50},
+
+            {"gift_total_1d", 51},
+            {"gift_total_3d", 52},
+            {"gift_total_7d", 53},
+            {"gift_total_15d", 54},
+            {"gift_total_30d", 55},
+
+            {"gift_time_1d", 56},
+            {"gift_time_3d", 57},
+            {"gift_time_7d", 58},
+            {"gift_time_15d", 59},
+            {"gift_time_30d", 60},
+
+            {"gift_combo_1d", 61},
+            {"gift_combo_3d", 62},
+            {"gift_combo_7d", 63},
+            {"gift_combo_15d", 64},
+            {"gift_combo_30d", 65},
+
+
+            {"click_rate_1d", 66},
+            {"click_rate_3d", 67},
+            {"click_rate_7d", 68},
+            {"click_rate_15d", 69},
+            {"click_rate_30d", 70},
+
+            {"avg_watch_time_1d", 71},
+            {"avg_watch_time_3d", 72},
+            {"avg_watch_time_7d", 73},
+            {"avg_watch_time_15d", 74},
+            {"avg_watch_time_30d", 75},
+
+            {"effect_enter_rate_1d", 76},
+            {"effect_enter_rate_3d", 77},
+            {"effect_enter_rate_7d", 78},
+            {"effect_enter_rate_15d", 79},
+            {"effect_enter_rate_30d", 80},
+
+            {"avg_heart_cnt_1d", 81},
+            {"avg_heart_cnt_3d", 82},
+            {"avg_heart_cnt_7d", 83},
+            {"avg_heart_cnt_15d", 84},
+            {"avg_heart_cnt_30d", 85},
+
+            {"avg_bullet_cnt_1d", 86},
+            {"avg_bullet_cnt_3d", 87},
+            {"avg_bullet_cnt_7d", 88},
+            {"avg_bullet_cnt_15d", 89},
+            {"avg_bullet_cnt_30d", 90},
+
+            {"avg_msg_cnt_1d", 91},
+            {"avg_msg_cnt_3d", 92},
+            {"avg_msg_cnt_7d", 93},
+            {"avg_msg_cnt_15d", 94},
+            {"avg_msg_cnt_30d", 95},
+
+            {"avg_follow_cnt_1d", 96},
+            {"avg_follow_cnt_3d", 97},
+            {"avg_follow_cnt_7d", 98},
+            {"avg_follow_cnt_15d", 99},
+            {"avg_follow_cnt_30d", 100},
+
+            {"avg_share_cnt_1d", 101},
+            {"avg_share_cnt_3d", 102},
+            {"avg_share_cnt_7d", 103},
+            {"avg_share_cnt_15d", 104},
+            {"avg_share_cnt_30d", 105},
+
+            {"avg_gift_cnt_1d", 106},
+            {"avg_gift_cnt_3d", 107},
+            {"avg_gift_cnt_7d", 108},
+            {"avg_gift_cnt_15d", 109},
+            {"avg_gift_cnt_30d", 110},
+
+            {"avg_gift_time_1d", 111},
+            {"avg_gift_time_3d", 112},
+            {"avg_gift_time_7d", 113},
+            {"avg_gift_time_15d", 114},
+            {"avg_gift_time_30d", 115}
+    };
+
+
+    std::cout << "---------------map.size=" << map.size() << std::endl;
+
+    int i = 1;
+    for (auto &item: map) {
+        std::cout << i << ": k=" << item.first << ", v=" << item.second << std::endl;
+        i++;
+    }
+}
+
 void SplitString(const std::string &s, std::vector<std::string> &v, const std::string &c) {
     std::string::size_type pos1, pos2;
     std::string::size_type c_size = c.size();
@@ -329,7 +481,7 @@ void test1() {
 
 int main(int argc, char **argv) {
     std::cout << "[Debug]" << std::endl;
-    test1();
+    outputMap303();
 
     return 0;
 }
